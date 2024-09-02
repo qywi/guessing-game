01fd178bbfd6f6b5cc8f5a3ecdf4a132e15f848b
Alexander <150794381+Maximus9991982@users.noreply.github.com>
2024-09-02 13:38:59 +0300

    feat: Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+    return this.result;
     }
 
     lower() {
-
+    this.min = this.min + (this.max - this.min) / 2;
     }
 
     greater() {
-
+    this.max = this.min + (this.max - this.min) / 2;
     }
 }
 

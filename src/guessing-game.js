f11a15ad61cb854647a2a8a1e75da2d6c95af434
qywi <teslagamnesla.123456789@gmail.com>
2024-09-02 20:57:20 +0300

    start task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentGuess = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentGuess = Math.floor((this.min + this.max) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess - 1;
     }
 
     greater() {
-
+        this.min = this.currentGuess + 1;
     }
 }
 
 module.exports = GuessingGame;
+
